decbc5c977d90cd97dc61133327b307fdaef0eb5
Craig Forrest <craigforrest@craigs-mbp.home>
2020-06-20 08:42:47 +0100

    initial commit

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  coverageThreshold: {
+    global: {
+      statements: 90,
+      branches: 90,
+      functions: 90,
+      lines: 90,
+    },
+  },
+  collectCoverageFrom: ['src/**/*.ts', '!src/index.ts'],
+};
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import App from './app';
+
+const app = App();
+const port = '5000';
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
